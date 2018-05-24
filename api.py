5e451bb3f26ca095c986942db8aeee47c8e51ba8
Simon Vialaret <simon.vialaret@orange.fr>
2018-05-24 13:56:29 +0200

    First commit

---
+++ api.py
@@ -0,0 +1,137 @@
+# -*- coding: utf-8 -*-
+
+import requests
+from datetime import datetime
+import hashlib
+import urllib
+import base64
+import random as rd
+
+
+allocine_secret_key = '29d185d98c984a359e6e6f26a0474269'
+allocine_partner = '100043982026'
+formatUrl = 'json'
+
+def init_connect():
+
+	### Initialisation : ip et user-agent aleatoire
+	## ip
+
+	ip = str(rd.randint(0, 255)) + '.' + str(rd.randint(0, 255)) + '.' + str(rd.randint(0, 255)) + '.' + str(rd.randint(0, 255))
+
+	## user-agent
+
+	v = str(rd.randint(1, 4)) + '.' + str(rd.randint(0,9))
+	a = str(rd.randint(0, 9))
+	b = str(rd.randint(0, 99))
+	c = str(rd.randint(0, 999))
+
+	userAgents = [
+		"Mozilla/5.0 (Linux; U; Android " + v + "; fr-fr; Nexus One Build/FRF91) AppleWebKit/5" + b + "." + c + " (KHTML, like Gecko) Version/" + a + "." + a + " Mobile Safari/5" + b + "." + c + "",
+	    "Mozilla/5.0 (Linux; U; Android " + v + "; fr-fr; Dell Streak Build/Donut AppleWebKit/5" + b + "." + c + "+ (KHTML, like Gecko) Version/3." + a + ".2 Mobile Safari/ 5" + b + "." + c + ".1",
+	    "Mozilla/5.0 (Linux; U; Android 4." + v + "; fr-fr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+	    "Mozilla/5.0 (Linux; U; Android 4." + v + "; fr-fr; HTC Sensation Build/IML74K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+	    "Mozilla/5.0 (Linux; U; Android " + v + "; en-gb) AppleWebKit/999+ (KHTML, like Gecko) Safari/9" + b + "." + a + "",
+	    "Mozilla/5.0 (Linux; U; Android " + v + ".5; fr-fr; HTC_IncredibleS_S710e Build/GRJ" + b + ") AppleWebKit/5" + b + ".1 (KHTML, like Gecko) Version/4.0 Mobile Safari/5" + b + ".1",
+	    "Mozilla/5.0 (Linux; U; Android 2." + v + "; fr-fr; HTC Vision Build/GRI" + b + ") AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+	    "Mozilla/5.0 (Linux; U; Android " + v + ".4; fr-fr; HTC Desire Build/GRJ" + b + ") AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+	    "Mozilla/5.0 (Linux; U; Android 2." + v + "; fr-fr; T-Mobile myTouch 3G Slide Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+	    "Mozilla/5.0 (Linux; U; Android " + v + ".3; fr-fr; HTC_Pyramid Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+	    "Mozilla/5.0 (Linux; U; Android 2." + v + "; fr-fr; HTC_Pyramid Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari",
+	    "Mozilla/5.0 (Linux; U; Android 2." + v + "; fr-fr; HTC Pyramid Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/5" + b + ".1",
+	    "Mozilla/5.0 (Linux; U; Android 2." + v + "; fr-fr; LG-LU3000 Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/5" + b + ".1",
+	    "Mozilla/5.0 (Linux; U; Android 2." + v + "; fr-fr; HTC_DesireS_S510e Build/GRI" + a + ") AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/" + c + ".1",
+	    "Mozilla/5.0 (Linux; U; Android 2." + v + "; fr-fr; HTC_DesireS_S510e Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile",
+	    "Mozilla/5.0 (Linux; U; Android " + v + ".3; fr-fr; HTC Desire Build/GRI" + a + ") AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+	    "Mozilla/5.0 (Linux; U; Android 2." + v + "; fr-fr; HTC Desire Build/FRF" + a + ") AppleWebKit/533.1 (KHTML, like Gecko) Version/" + a + ".0 Mobile Safari/533.1",
+	    "Mozilla/5.0 (Linux; U; Android " + v + "; fr-lu; HTC Legend Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/" + a + "." + a + " Mobile Safari/" + c + "." + a + "",
+	    "Mozilla/5.0 (Linux; U; Android " + v + "; fr-fr; HTC_DesireHD_A9191 Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1",
+	    "Mozilla/5.0 (Linux; U; Android " + v + ".1; fr-fr; HTC_DesireZ_A7" + c + " Build/FRG83D) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/" + c + "." + a + "",
+	    "Mozilla/5.0 (Linux; U; Android " + v + ".1; en-gb; HTC_DesireZ_A7272 Build/FRG83D) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/" + c + ".1",
+	    "Mozilla/5.0 (Linux; U; Android " + v + "; fr-fr; LG-P5" + b + " Build/FRG83) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"
+	]
+
+	headersUA = {'user-agent' : userAgents[rd.randint(0,len(userAgents)-1)]}
+
+	return (ip, headersUA)
+
+def getDate():
+	dateComplete = datetime.now()
+	year = dateComplete.year
+	month = dateComplete.month
+	day = dateComplete.day
+
+	if month < 10:
+		YMDstr = str(year) + '0' + str(month) + str(day)
+
+	else:
+		YMDstr = str(year) + str(month) + str(day)
+	return YMDstr
+
+def cityDesc(city):
+
+	ip, headersUA = init_connect()
+	YMDstr = getDate()
+
+	searchField = city
+	filterField = ''
+	countField = '500'
+	pageField = '1'
+
+	url = 'q=' + searchField + '&filter=' + filterField + '&count=' + countField + '&page=' + pageField + '&format=json&partner=' + allocine_partner + '&sed=' + YMDstr
+
+	toEncrypt = allocine_secret_key + url
+
+	sig = urllib.quote_plus(base64.b64encode(hashlib.sha1(toEncrypt).digest()))
+
+	urlComplete = 'http://api.allocine.fr/rest/v3/search?' + url + "&sig=" + sig
+
+	# print(urlComplete)
+	
+	req = requests.get(urlComplete, headers=headersUA)
+	# print(req.status_code)
+	# print(req.text)
+
+	listeCine = []
+	codePostal = 0
+
+	if req.status_code == 200:
+
+		codePostal = req.json()['feed']['location'][0]['postalCode']
+
+		if 'theater' in req.json()['feed']:
+			for theaterCity in req.json()['feed']['theater']:
+				listeCine.append(theaterCity)
+		else:
+			print('Pas de salle de cinema a ' + searchField)
+
+	else:
+		print("La requete n'a pas abouti")
+
+	return codePostal, listeCine
+
+def showtimeInTheater(codeTheater, jour):
+	ip, headersUA = init_connect()
+	YMDstr = getDate()
+
+	searchField = codeTheater
+	filterField = ''
+	countField = '500'
+	pageField = '1'
+
+	url = 'theaters=' + searchField + '&date='+ jour + '&filter=' + filterField + '&count=' + countField + '&page=' + pageField + '&format=json&partner=' + allocine_partner + '&sed=' + YMDstr
+
+	toEncrypt = allocine_secret_key + url
+
+	sig = urllib.quote_plus(base64.b64encode(hashlib.sha1(toEncrypt).digest()))
+
+	urlComplete = 'http://api.allocine.fr/rest/v3/showtimelist?' + url + "&sig=" + sig
+
+	# print(urlComplete)
+	
+	req = requests.get(urlComplete, headers=headersUA)
+
+	# print(req.status_code)
+	# print(req.text)
+
+	return req.json()['feed']
---
+++ main.py
@@ -0,0 +1,93 @@
+import sys, os
+os.chdir('/home/rosaell/Prog/allocine/plan-your-movies')
+from PyQt5.QtWidgets import QGroupBox, QComboBox, QDateEdit, QCalendarWidget, QWidget, QApplication, QStackedLayout, QLabel, QLineEdit, QPushButton, QHBoxLayout, QVBoxLayout
+from PyQt5.QtCore import QDate
+from api import *
+
+
+class Principale(QWidget):
+	def __init__(self):
+		super(Principale, self).__init__()
+		self.setUI()
+	
+	def setUI(self):
+
+		labelVille = QLabel("Ville : ")
+		labelJour = QLabel("Jour : ")
+		textEditVille = QLineEdit()
+		calendarJour = QDateEdit()
+		calendarJour.setCalendarPopup(True)
+		calendarJour.setDate(QDate.currentDate())
+		buttonValiderParam = QPushButton("Valider")
+		groupBoxSeances = QGroupBox()
+
+		hboxParam = QHBoxLayout()
+		vboxGlobal = QVBoxLayout()
+		vboxSeances = QVBoxLayout()
+
+		hboxParam.addWidget(labelVille)
+		hboxParam.addWidget(textEditVille)
+		hboxParam.addWidget(labelJour)
+		hboxParam.addWidget(calendarJour)
+		hboxParam.addWidget(buttonValiderParam)
+
+		vboxGlobal.addLayout(hboxParam)
+		vboxGlobal.addLayout(vboxSeances)
+
+		self.setLayout(vboxGlobal)
+		
+		buttonValiderParam.clicked.connect(lambda: self.validerParam(textEditVille.text(), calendarJour.date()))
+
+		self.setWindowTitle('Super Cinemon 2000 ExtraPlus')
+		
+		self.show()
+
+	def validerParam(self, ville, jour):
+		year = jour.year()
+		month = jour.month()
+		day = jour.day()
+
+		if month < 10:
+			jourCine = str(year) + '-' + '0' + str(month) + '-' + str(day)
+
+		else:
+			jourCine = str(year) + '-' + str(month) + '-' + str(day)
+		CP, listeCine = cityDesc(ville)
+
+		dicoHoraires = dict()
+
+		for cine in listeCine:
+			seanceVille = showtimeInTheater(cine['code'], jourCine)
+
+			if 'movieShowtimes' in seanceVille['theaterShowtimes'][0]:
+
+				for film in seanceVille['theaterShowtimes'][0]['movieShowtimes']:
+					# print(film['onShow']['movie']['title'])
+					for seance in film['scr']:
+						if seance['d'] == jourCine:
+							for occurSeance in seance['t']:
+
+								if not (film['onShow']['movie']['title'] in dicoHoraires):
+									dicoHoraires[film['onShow']['movie']['title']] = []
+									dicoHoraires[film['onShow']['movie']['title']].append(dict())
+									dicoHoraires[film['onShow']['movie']['title']][-1]['salle'] = cine['name']
+									dicoHoraires[film['onShow']['movie']['title']][-1]['code'] = cine['code']
+									dicoHoraires[film['onShow']['movie']['title']][-1]['horaire'] = occurSeance['$']
+								else:
+									dicoHoraires[film['onShow']['movie']['title']].append(dict())
+									dicoHoraires[film['onShow']['movie']['title']][-1]['salle'] = cine['name']
+									dicoHoraires[film['onShow']['movie']['title']][-1]['code'] = cine['code']
+									dicoHoraires[film['onShow']['movie']['title']][-1]['horaire'] = occurSeance['$']
+		
+		for film in dicoHoraires:
+			dicoHoraires[film] = sorted(dicoHoraires[film], key=lambda t:t['horaire'])
+			groupBoxHoraire = QGroupBox(film)
+			vboxHor = QVBoxLayout()
+			for hor in dicoHoraires[film]:
+				vboxHor.addWidget(QLabel(hor['salle'] + ' : ' + hor['horaire']))
+			groupBoxHoraire.setLayout(vboxHor)
+			self.layout().itemAt(1).addWidget(groupBoxHoraire)
+
+monApp=QApplication(sys.argv)
+fenetre=Principale()
+sys.exit(monApp.exec_())
